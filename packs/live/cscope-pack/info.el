15c8151b3fac6e7c1a34029c97d80c15fa9434f7
debugger87 <yangchaozhong.2009@gmail.com>
2014-01-27 15:44:57 +0800

    cscope-pack

---
+++ packs/live/cscope-pack/info.el
@@ -0,0 +1,3 @@
+(live-pack-name "cscope-pack")
+(live-pack-version "0.0.1alpha")
+(live-pack-description "cscope-pack, help you navigate in c/c++ project.")
---
+++ packs/live/cscope-pack/init.el
@@ -0,0 +1,13 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(load ((concat (live-pack-lib-dir) "xcscope.el")))
+
+(add-hook 'c-mode-common-hook
+          '(lambda ()
+             (require 'xcscope)))
+
+(setq cscope-do-not-update-database t)

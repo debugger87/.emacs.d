61beb5380308108953baef8d96b2f2dad5f70b27
debugger87 <yangchaozhong.2009@gmail.com>
2014-03-18 14:51:45 +0800

    FIX

--- packs/live/csharp-pack/init.el
+++ packs/live/csharp-pack/init.el
@@ -3,7 +3,7 @@
 ;; Use this file to initiate the pack configuration.
 ;; See README for more information.
 
-(load (concat (live-packs-dir) "csharp-mode.el"))
+(load (concat (live-pack-lib-dir) "csharp-mode.el"))
 
 (add-hook 'csharp-mode
           '(lambda ()
